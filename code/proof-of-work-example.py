f50e66a867e843fa41203e5687dc370373de926c
Andreas M. Antonopoulos <andreas@antonopoulos.com>
2013-09-07 14:11:42 -0700

    code improvement on PoW and some new writeup

--- code/proof-of-work-example.py
+++ code/proof-of-work-example.py
@@ -1,14 +1,14 @@
-
+#!/usr/bin/env python
 # example of proof-of-work algorithm
 
 import hashlib
 import time
 
-max_nonce = 1000000000  # 1 billion
+max_nonce = 2 ** 32 # 4 billion
 
-def proof_of_work(header, difficulty):
+def proof_of_work(header, difficulty_bits):
     
-    target = 2<<(256-difficulty)
+    target = 2 ** (256-difficulty_bits)
     
     for nonce in xrange(max_nonce):
         hash_result = hashlib.sha256(str(header)+str(nonce)).hexdigest()
@@ -16,7 +16,7 @@ def proof_of_work(header, difficulty):
         if long(hash_result, 16) < target:
             print "Success with nonce %d" % nonce
             print "Hash is %s" % hash_result
-            return nonce
+            return (hash_result,nonce)
             
     print "Failed after %d (max_nonce) tries" % nonce
     return nonce
@@ -24,22 +24,25 @@ def proof_of_work(header, difficulty):
     
 if __name__ == '__main__':
     
-    difficulty = 24
+    nonce = 0
+    hash_result = ''
+        
+    for difficulty_bits in xrange(32):
+        
+        difficulty = 2 ** difficulty_bits
+        print "Difficulty: %ld (%d bits)" % (difficulty, difficulty_bits)
     
-    print "Difficulty: %d" % difficulty
+        print "Starting search..."
+        start_time = time.time()
+        new_block = 'test block with transactions' + hash_result # make a new block which includes the hash from the previous block
+        (hash_result, nonce) = proof_of_work(new_block, difficulty_bits) # find a nonce for the new block
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print "Elapsed Time: %.4f seconds" % elapsed_time
     
-    print "Starting search..."
-    start_time = time.time()
-    
-    nonce = proof_of_work('test header', difficulty) # 5 bits of difficulty
-    
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print "Elapsed Time: %.2f seconds" % elapsed_time
-    
-    if elapsed_time > 0:
-        hash_power = float(long(nonce)/elapsed_time)
-        print "Hashing Power: %ld hashes per second" % hash_power
+        if elapsed_time > 0:
+            hash_power = float(long(nonce)/elapsed_time)
+            print "Hashing Power: %ld hashes per second" % hash_power
     
     
     
