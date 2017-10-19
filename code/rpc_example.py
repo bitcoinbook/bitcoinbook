fa6ee0557e219011ee34093447beaedc851123a6
MarcoFalke <falke.marco@gmail.com>
2017-10-19 22:38:27 +0200

    Rework ch03 for most recent version of Bitcoin Core

--- code/rpc_example.py
+++ code/rpc_example.py
@@ -3,8 +3,8 @@ from bitcoin.rpc import RawProxy
 # Create a connection to local Bitcoin Core node
 p = RawProxy()
 
-# Run the getinfo command, store the resulting data in info
-info = p.getinfo()
+# Run the getblockchaininfo command, store the resulting data in info
+info = p.getblockchaininfo()
 
 # Retrieve the 'blocks' element from the info
 print(info['blocks'])
