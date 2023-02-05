5f2c61e71b820e3991d1b510d08e72b9600bebea
David A. Harding <dave@dtrt.org>
2023-02-05 07:02:41 -1000

    CH03::shell & code examples: update for 2023

--- code/rpc_transaction.py
+++ code/rpc_transaction.py
@@ -3,7 +3,7 @@ from bitcoin.rpc import RawProxy
 p = RawProxy()
 
 # Alice's transaction ID
-txid = "0627052b6f28912f2703066a912ea577f2ce4da4caa5a5fbd8a57286c345c2f2"
+txid = "466200308696215bbc949d5141a49a4138ecdfdfaa2a8029c1f9bcecd1f96177"
 
 # First, retrieve the raw transaction in hex
 raw_tx = p.getrawtransaction(txid)
@@ -13,4 +13,4 @@ decoded_tx = p.decoderawtransaction(raw_tx)
 
 # Retrieve each of the outputs from the transaction
 for output in decoded_tx['vout']:
-    print(output['scriptPubKey']['addresses'], output['value'])
+    print(output['scriptPubKey']['address'], output['value'])
