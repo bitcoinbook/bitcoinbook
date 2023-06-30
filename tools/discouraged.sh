5215e8c3e845a0ae3896d0bed938a9b582e2b877
David A. Harding <dave@dtrt.org>
2023-06-30 13:41:20 -1000

    Tool: add discouraged.sh to find discouraged words & phrases

---
+++ tools/discouraged.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -u
+
+## Main discouraged words
+for f in $( git ls-files | grep -v "$0" ) ; do
+  # 1. Find discouraged words
+  # 2. Ignore things that look like json or code (Bitcoin Core RPCs use many discouraged words)
+  egrep -if <( sed "1,/[S]TART DISCOURAGED WORDS/d" "$0" ) "$f" \
+    | grep -v "[\"'][a-zA-Z]\+[\"']" \
+    | if grep . ; then
+      echo "DISCOURAGED WORDS FOUND IN $f"
+    fi
+done
+
+## Discouraged words that require special greps
+for f in $( git ls-files | grep -v "$0" ) ; do
+  {
+  echo -n
+  } | if grep . ; then echo "DISCOURAGED WORDS FOUND IN $f" ; exit 1 ; fi
+done
+
+exit
+## START DISCOURAGED WORDS
+BIP-[1-9]
+BIP0
+BIP [0-9]
+\<nversion\>
+\<nsequence\>
+\<nlocktime\>
+\<locktime\>
+\<vin\>
+\<vout\>
+\<scriptSig\>
+\<scriptPubKey\>
+\<redeemScript\>
+\<nAmount\>
+\<nValue\>
+witness field
+witness element
